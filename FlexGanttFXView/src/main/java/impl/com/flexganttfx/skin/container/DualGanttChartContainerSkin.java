54e0b181ff98ffd58835542c1a314c85d0cf3a97
Dirk Lemmermann <dlemmermann@gmail.com>
2020-05-08 18:02:08 +0200

    Code cleanup.

--- FlexGanttFXView/src/main/java/impl/com/flexganttfx/skin/container/DualGanttChartContainerSkin.java
+++ FlexGanttFXView/src/main/java/impl/com/flexganttfx/skin/container/DualGanttChartContainerSkin.java
@@ -1,55 +1,50 @@
 /**
  * Copyright (C) 2014 - 2020 DLSC Software & Consulting GmbH (dlsc.com)
- *
+ * <p>
  * This file is part of FlexGanttFX.
  */
 package impl.com.flexganttfx.skin.container;
 
 import com.flexganttfx.view.container.DualGanttChartContainerBase;
-import org.controlsfx.control.MasterDetailPane;
-
 import javafx.beans.binding.Bindings;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.BorderPane;
+import org.controlsfx.control.MasterDetailPane;
 
 public class DualGanttChartContainerSkin extends MultiGanttChartContainerSkinBase<DualGanttChartContainerBase> {
 
-	public DualGanttChartContainerSkin(DualGanttChartContainerBase container) {
-		super(container);
+    public DualGanttChartContainerSkin(DualGanttChartContainerBase container) {
+        super(container);
 
-		MasterDetailPane masterDetailPane = container.getMasterDetailPane();
-		masterDetailPane.setMasterNode(new Wrapper(true));
-		masterDetailPane.setDetailNode(new Wrapper(false));
-		masterDetailPane.setDividerPosition(.6);
+        MasterDetailPane masterDetailPane = container.getMasterDetailPane();
+        masterDetailPane.setMasterNode(new Wrapper(true));
+        masterDetailPane.setDetailNode(new Wrapper(false));
+        masterDetailPane.setDividerPosition(.6);
 
-		masterDetailPane.setShowDetailNode(container.isShowSecondary());
-		Bindings.bindBidirectional(container.showSecondaryProperty(),
-				masterDetailPane.showDetailNodeProperty());
+        masterDetailPane.setShowDetailNode(container.isShowSecondary());
+        Bindings.bindBidirectional(container.showSecondaryProperty(), masterDetailPane.showDetailNodeProperty());
 
-		getChildren().add(masterDetailPane);
-	}
+        getChildren().add(masterDetailPane);
+    }
 
-	private class Wrapper extends BorderPane {
+    private class Wrapper extends BorderPane {
 
-		public Wrapper(boolean primary) {
-			if (primary) {
-				topProperty().bind(getSkinnable().primaryHeaderProperty());
-				centerProperty().bind(
-						getSkinnable().primaryGanttChartProperty());
-				bottomProperty().bind(getSkinnable().primaryFooterProperty());
-			} else {
-				topProperty().bind(getSkinnable().secondaryHeaderProperty());
-				centerProperty().bind(
-						getSkinnable().secondaryGanttChartProperty());
-				bottomProperty().bind(getSkinnable().secondaryFooterProperty());
+        public Wrapper(boolean primary) {
+            if (primary) {
 
-				getSkinnable().secondaryGanttChartProperty()
-						.addListener(
-								observable -> SplitPane
-										.setResizableWithParent(getSkinnable()
-												.getSecondaryGanttChart(),
-												false));
-			}
-		}
-	}
+            	topProperty().bind(getSkinnable().primaryHeaderProperty());
+                centerProperty().bind(getSkinnable().primaryGanttChartProperty());
+                bottomProperty().bind(getSkinnable().primaryFooterProperty());
+
+            } else {
+
+            	topProperty().bind(getSkinnable().secondaryHeaderProperty());
+                centerProperty().bind(getSkinnable().secondaryGanttChartProperty());
+                bottomProperty().bind(getSkinnable().secondaryFooterProperty());
+
+                getSkinnable().secondaryGanttChartProperty().addListener(observable -> SplitPane.setResizableWithParent(getSkinnable().getSecondaryGanttChart(), false));
+
+            }
+        }
+    }
 }
