bcc231e72adbc35da93b4541790aec1262a32a3e
Dirk Lemmermann <dlemmermann@gmail.com>
2020-05-12 15:32:59 +0200

    Added build for msproject demo on Mac.

---
+++ FlexGanttFXMSProject/src/main/java/com/flexganttfx/msproject/MSProjectAppLauncher.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C) 2014 - 2020 DLSC Software & Consulting GmbH (dlsc.com)
+ * <p>
+ * This file is part of FlexGanttFX.
+ */
+package com.flexganttfx.msproject;
+
+public class MSProjectAppLauncher {
+
+    public static void main(String[] args) {
+        MSProjectApp.main(args);
+    }
+}
