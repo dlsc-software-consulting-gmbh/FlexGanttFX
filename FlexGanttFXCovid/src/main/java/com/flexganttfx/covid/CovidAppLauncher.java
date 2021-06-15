6f615eb6d70cb189e81df70c9c984263382d3394
Dirk Lemmermann <dlemmermann@gmail.com>
2021-06-15 14:16:23 +0200

    Added native build for Covid explorer.

---
+++ FlexGanttFXCovid/src/main/java/com/flexganttfx/covid/CovidAppLauncher.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C) 2014 - 2021 DLSC Software & Consulting GmbH (dlsc.com)
+ *
+ * This file is part of FlexGanttFX.
+ */
+package com.flexganttfx.covid;
+
+public class CovidAppLauncher {
+
+    public static void main(String[] args) {
+        CovidApp.main(args);
+    }
+}
