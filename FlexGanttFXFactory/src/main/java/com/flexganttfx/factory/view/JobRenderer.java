13e001fefa24c217ebc12e60531e111936c42f01
dlemmermann <dlemmermann@gmail.com>
2026-03-20 16:10:04 +0100

    Add new `FlexGanttFXFactory` module with a factory scheduling demo, including data model, Gantt chart, and UI components.

---
+++ FlexGanttFXFactory/src/main/java/com/flexganttfx/factory/view/JobRenderer.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (C) 2014 - 2026 DLSC Software & Consulting GmbH (dlsc.com)
+ * This file is part of FlexGanttFX.
+ */
+package com.flexganttfx.factory.view;
+
+import com.flexganttfx.factory.model.Job;
+import com.flexganttfx.factory.model.JobStatus;
+import com.flexganttfx.model.ActivityRef;
+import com.flexganttfx.view.graphics.ActivityBounds;
+import com.flexganttfx.view.graphics.GraphicsBase;
+import com.flexganttfx.view.graphics.renderer.CompletableActivityRenderer;
+import com.flexganttfx.view.util.Position;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+import java.util.EnumMap;
+
+/**
+ * Renders {@link Job} activities as colour-coded bars based on their
+ * {@link JobStatus}. A semi-transparent overlay drawn by the parent
+ * {@link CompletableActivityRenderer} shows the percentage complete. The job
+ * name is drawn in the centre of the bar.
+ */
+public class JobRenderer extends CompletableActivityRenderer<Job> {
+
+    private static final EnumMap<JobStatus, Color> FILL_MAP = new EnumMap<>(JobStatus.class);
+    private static final EnumMap<JobStatus, Color> STROKE_MAP = new EnumMap<>(JobStatus.class);
+
+    static {
+        FILL_MAP.put(JobStatus.SCHEDULED,   Color.STEELBLUE);
+        FILL_MAP.put(JobStatus.IN_PROGRESS, Color.DARKORANGE);
+        FILL_MAP.put(JobStatus.DONE,        Color.MEDIUMSEAGREEN);
+        FILL_MAP.put(JobStatus.DELAYED,     Color.CRIMSON);
+
+        STROKE_MAP.put(JobStatus.SCHEDULED,   Color.STEELBLUE.darker());
+        STROKE_MAP.put(JobStatus.IN_PROGRESS, Color.DARKORANGE.darker());
+        STROKE_MAP.put(JobStatus.DONE,        Color.MEDIUMSEAGREEN.darker());
+        STROKE_MAP.put(JobStatus.DELAYED,     Color.CRIMSON.darker());
+    }
+
+    public JobRenderer(GraphicsBase<?> graphics) {
+        super(graphics, "Job");
+        setCornersRounded(true);
+        setFillCompletion(new Color(0, 0, 0, 0.25));
+    }
+
+    @Override
+    public ActivityBounds drawActivity(ActivityRef<Job> activityRef,
+                                       Position position, GraphicsContext gc,
+                                       double x, double y, double w, double h,
+                                       boolean selected, boolean hover,
+                                       boolean highlighted, boolean pressed) {
+
+        JobStatus status = activityRef.getActivity().getStatus();
+        setFill(FILL_MAP.getOrDefault(status, Color.GRAY));
+        setStroke(STROKE_MAP.getOrDefault(status, Color.DARKGRAY));
+
+        ActivityBounds bounds = super.drawActivity(activityRef, position, gc,
+                x, y, w, h, selected, hover, highlighted, pressed);
+
+        drawText(activityRef, activityRef.getActivity().getName(),
+                TextPosition.CENTER, gc, x, y, w, h,
+                selected, hover, highlighted, pressed);
+
+        return bounds;
+    }
+}
---
+++ FlexGanttFXFactory/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) 2014 - 2026 DLSC Software & Consulting GmbH (dlsc.com)
+ * This file is part of FlexGanttFX.
+ */
+module com.flexganttfx.factory {
+
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+
+    requires com.flexganttfx.view;
+    requires com.flexganttfx.extras;
+
+    exports com.flexganttfx.factory;
+    exports com.flexganttfx.factory.model;
+    exports com.flexganttfx.factory.view;
+}
